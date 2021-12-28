46a5b00d4003f37d02a547ec16c71f04e004d42d
Dirk Peters <dirk.peters.ath@gmx.de>
2021-12-28 15:13:25 +0100

    enable log following

--- automation/DockerCli.Testbed/Program.cs
+++ automation/DockerCli.Testbed/Program.cs
@@ -1,6 +1,22 @@
-﻿using DockerCli.Testbed;
+﻿using System;
+using System.Threading.Tasks;
+using DockerCli.Testbed;
+using Console = System.Console;
 
 var host = new DockerHost();
 var container = await host.Run(new ImageName("hello-world", "latest"));
-await container.Log();
+var isRunning = true;
+var log = await container.LogAndFollow();
+
+using var subscription = log.Subscribe(
+    Console.WriteLine,
+    error => Console.Error.WriteLine(error),
+    () => isRunning = false);
+
+while (isRunning)
+{
+    await Task.Yield();
+}
+
 await container.Remove();
+await Console.Out.FlushAsync();
